a0b2e2723ba7306b1320c8e9021eb540e1f23c40
kwingham <kyle_wingham@outlook.com>
2024-09-26 09:44:23 +0100

    Updated  message form

--- client/src/components/MessageForm.jsx
+++ client/src/components/MessageForm.jsx
@@ -1,52 +1,52 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export function MessageForm() {
-    const [formValues, setFormValues] = useState ({
-        name: "",
-        message: "",
-    }) //inital values
+  const [formValues, setFormValues] = useState({
+    name: "",
+    message: "",
+  }); //inital values
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const res = await fetch(`http://localhost:8080/comments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formValues),
+    });
+    const messages = await res.json();
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        async function fetchMessages() {
-            const res = await fetch(`http://localhost:8080/comments`)
-            const messages = await res.json() 
-            
-        }
+  function handleInputChange(event) {
+    setFormValues({ ...formValues, [event.target.name]: event.target.value });
+  }
 
-        
-    }
-
-    function handleInputChange(event) {
-        setFormValues({...formValues, [event.target.name]: event.target.value})
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input
-                type='text'
-                id='name'
-                name='name'
-                placeholder='Your name here'
-                value={formValues.name}
-                onChange={handleInputChange}
-            />
-            <label htmlFor='message'>Message</label>
-            <input
-                type='text'
-                id='message'
-                name='message'
-                placeholder='Enter message'
-                value={formValues.message}
-                onChange={handleInputChange}
-            />
-            <p>Current value is: {formValues.name}</p>
-            <p>Current value is: {formValues.message}</p>
-            <button type='submit'>Submit</button>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Your name here"
+        value={formValues.name}
+        onChange={handleInputChange}
+      />
+      <label htmlFor="message">Message</label>
+      <input
+        type="text"
+        id="message"
+        name="message"
+        placeholder="Enter message"
+        value={formValues.message}
+        onChange={handleInputChange}
+      />
+      <p>Current value is: {formValues.name}</p>
+      <p>Current value is: {formValues.message}</p>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default MessageForm;
